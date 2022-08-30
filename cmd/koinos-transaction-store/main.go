28802c0f3534bcab7c271d2a3c6e92f5522bddcf
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2022-08-30 11:25:14 -0700

    Fix basedir assignment

--- cmd/koinos-transaction-store/main.go
+++ cmd/koinos-transaction-store/main.go
@@ -54,7 +54,7 @@ func main() {
 
 	var baseDir string
 
-	baseDir = flag.StringP(basedirOption, "d", basedirDefault, "Koinos base directory")
+	baseDir = *flag.StringP(basedirOption, "d", basedirDefault, "Koinos base directory")
 	amqp := flag.StringP(amqpOption, "a", "", "AMQP server URL")
 	reset := flag.BoolP("reset", "r", false, "Reset the database")
 	instanceID := flag.StringP(instanceIDOption, "i", instanceIDDefault, "The instance ID to identify this service")
